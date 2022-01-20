982237a73005787e7f77b16a449731005c449669
Leonardo Tereza <leonardoctereza@gmail.com>
2022-01-19 21:40:04 -0300

    remove build

--- build/services/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculatorService = void 0;
-var calculator_service_1 = require("./calculator.service");
-var calculatorService = new calculator_service_1.CalculatorService();
-exports.calculatorService = calculatorService;
